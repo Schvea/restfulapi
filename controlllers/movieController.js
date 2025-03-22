3b36bbee21966fb852be4fcfede2035437520a40
scvhea <svea.holm03@gmail.com>
2025-03-22 21:29:48 +0100

    test funktion addera film

--- controlllers/movieController.js
+++ controlllers/movieController.js
@@ -1,2 +1,34 @@
 import Movie from "../models/movieModel.js";
 
+async function getFiveRandomMovies(req, res) {
+    try{
+        const movies = await Movie.find({}).limit($)
+        .select("title genre created director");
+
+        res.json(movies)
+    } catch (error){
+        res.status(500).json({message: error.message});
+    }
+}
+async function getMovieByID(req, res){
+
+}
+async function createMovie(req, res) {
+    const { title, genre, created, director } = req.body;
+
+    try {
+        const newMovie = new Movie({
+            title,
+            genre,
+            created,
+            director,
+        });
+
+        const savedMovie = await newMovie.save();
+        res.status(201).json(savedMovie);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
+
+export {getFiveRandomMovies, getMovieByID, createMovie};
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,8 +1,9 @@
 import{ Router } from "express";
+import { getFiveRandomMovies, getMovieByID } from "../controlllers/movieController.js";
 
 const movieRouter = Router();
 
-movieRouter.get("/", /*getFiveRandomMovies*/);
+movieRouter.get("/", getFiveRandomMovies);
 
 movieRouter.get('/:movieID', /*getMovieByID*/);
 
