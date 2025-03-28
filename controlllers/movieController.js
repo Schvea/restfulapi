ce526546c767fd5ef304c43c637f55b68ae1c33d
scvhea <svea.holm03@gmail.com>
2025-03-28 19:33:03 +0100

    crud klar

--- controlllers/movieController.js
+++ controlllers/movieController.js
@@ -29,4 +29,40 @@ async function createMovie(req, res) {
     }
 }
 
-export {getAllMovies, createMovie};
+async function updateMovie(req, res) {
+    const { movieID } = req.params; 
+    const { title, genre, created, director } = req.body;
+
+    try {
+       
+        const updatedMovie = await Movie.findByIdAndUpdate(
+            movieID, 
+            { title, genre, created, director },
+            { new: true }
+        );
+
+        if (!updatedMovie) {
+            return res.status(404).json({ message: "Film hittades ej" });
+        }
+
+        res.json(updatedMovie);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
+async function deleteMovie(req, res) {
+    const { movieID } = req.params;
+
+    try {
+        const deletedMovie = await Movie.findByIdAndDelete(movieID);
+
+        if (!deletedMovie) {
+            return res.status(404).json({ message: "film hittades ej" });
+        }
+
+        res.json({ message: "Filmen är borttagen", movie: deletedMovie });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+}
+export {getAllMovies, createMovie, updateMovie, deleteMovie };
--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -1,10 +1,14 @@
 import{ Router } from "express";
-import { getAllMovies, createMovie } from "../controlllers/movieController.js";
+import { getAllMovies, createMovie, updateMovie, deleteMovie } from "../controlllers/movieController.js";
 
 const movieRouter = Router();
 
 movieRouter.get("/", getAllMovies);
 
 movieRouter.post("/", createMovie);
+
+movieRouter.put("/:movieID", updateMovie); 
+
+movieRouter.delete("/:movieID", deleteMovie);
 
 export default movieRouter;
