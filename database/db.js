82be914d7f0b4cd3702010f166d181750d0ca711
scvhea <svea.holm03@gmail.com>
2025-04-14 15:26:24 +0200

    test curl

--- database/db.js
+++ database/db.js
@@ -2,9 +2,13 @@ import mongoose from "mongoose";
 
 export default async function connectDB(){
     try{
-        await mongoose.connect(process.env.MONGO_URI);
-
-        console.log('[server]: DB conncected successfully');
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 10000, 
+          });
+      
+          console.log(`[server]: DB ${conn.connection.host}`);
     } catch (error){
         console.error(`[error]: ${error.message}`);
     }
