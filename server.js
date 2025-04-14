259a1f594cbb4d95735a2929ced920a3124a69bc
scvhea <svea.holm03@gmail.com>
2025-04-14 15:09:18 +0200

    test

--- server.js
+++ server.js
@@ -1,27 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
-import { get } from "mongoose";
+import mongoose from "mongoose";
 import connectDB from "./database/db.js";
 import movieRouter from "./routes/movieRoutes.js";
 import cors from 'cors';
 
-
-
 dotenv.config();
-connectDB();
-const port = process.env.PORT || 3000;
-
-console.log(`Server running on port: ${port}`);
 
 const app = express ();
 
+const uri = process.env.MONGO_URI;
+
+connectDB();
+
+app.use(cors());
 app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+console.log(`Server running on port: ${port}`);
 
 app.get('/', (req, res) => res.send("hej"))
 
 app.use("/api/movies", movieRouter);
 
-app.use(cors());
 
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
